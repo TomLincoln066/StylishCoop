4a47ba39cc901e3f63c23e3f5a1078072d4142f5
Will Lo <darthhun54@gmail.com>
2020-01-09 14:05:56 +0800

    add a new viewModel for UserLoginDialog and app works

--- app/src/main/java/app/appworks/school/stylish/userlogin/userLoginDialog.kt
+++ app/src/main/java/app/appworks/school/stylish/userlogin/userLoginDialog.kt
@@ -1,2 +1,37 @@
 package app.appworks.school.stylish.userlogin
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.fragment.app.viewModels
+import app.appworks.school.stylish.databinding.DialogUserLoginAlertBinding
+import app.appworks.school.stylish.ext.getVmFactory
+
+
+class userLoginDialog : AppCompatDialogFragment() {
+
+    /**
+     * Lazily initialize our [ProfileViewModel].
+     */
+    private val viewModel by viewModels<UserLoginDialogViewModel> { getVmFactory() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding = DialogUserLoginAlertBinding.inflate(inflater, container, false)
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+
+        return binding.root
+
+    }
+
+
+}
